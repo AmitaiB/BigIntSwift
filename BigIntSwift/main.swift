8b34295240c3419abb0f0a01674ec905470f0eb2
AmitaiB <blickstein@gmail.com>
2018-02-27 22:20:24 -0500

    Initial Commit

---
+++ BigIntSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BigIntSwift
+//
+//  Created by Amitai Blickstein on 2/27/18.
+//  Copyright © 2018 Amitai Blickstein. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
